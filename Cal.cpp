6d5c0c9ed878c791903c2a42de55723197e35edd
techdhaba-swdev <techdhaba.swdev@gmail.com>
2022-11-14 12:21:54 +0530

    created new file to sub/mul/div of two int num

--- Cal.cpp
+++ Cal.cpp
@@ -9,13 +9,31 @@ class Cal{
 			y = 0;
 		}
 		int add(int,int);
+		int sub(int,int);
+		int mul(int,int);
+		int div(int,int);
 };
 int Cal::add(int x,int y){
 	return x+y;
 }
 
+int Cal::sub(int x,int y){
+	return x-y;
+}
+int Cal::mul(int x,int y){
+	return x*y;
+}
+int Cal::div(int x,int y){
+	if (y>0)
+		return x/y;
+	else
+		return 0;
+}
 int main(){
 	Cal obj;
 	cout<<"add is "<<obj.add(4,6)<<endl;
+	cout<<"sub is "<<obj.sub(6,2)<<endl;
+	cout<<"mul is "<<obj.mul(5,4)<<endl;
+	cout<<"div is "<<obj.div(30,5)<<endl;
 return 0;
 }
